79cad2a4880d6ee86d1f62b604d0bed999f7154d
oscar <oscarmore2@gmail.com>
2018-05-13 15:16:11 +0800

    embed structure.

---
+++ SspUnityProject/Assets/Script/Resource/IResource.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ResourceType
+{
+
+}
+
+interface IResource  {
+
+    ResourceType GetType();
+    string GetPath();
+
+}
