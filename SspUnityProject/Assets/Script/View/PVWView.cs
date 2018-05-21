50a742e33984b1de5083513a25bd754714b9f668
oscar <oscarmore2@gmail.com>
2018-05-21 18:26:25 +0800

    add view related

--- SspUnityProject/Assets/Script/View/PVWView.cs
+++ SspUnityProject/Assets/Script/View/PVWView.cs
@@ -1,7 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PVWView : IView {
+    Texture pvwBuffer;
+    RenderProcessManager renderProcessManager;
 
+    protected override void OnHided()
+    {
+
+    }
+
+    protected override void OnShown()
+    {
+
+    }
 }
