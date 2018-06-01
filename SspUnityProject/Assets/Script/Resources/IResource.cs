6f676625f40d889461026bf6da85c9326e82c240
Oscar <oscar@anthivegames.com>
2018-06-01 09:16:34 +0800

    add resource logic

--- SspUnityProject/Assets/Script/Resources/IResource.cs
+++ SspUnityProject/Assets/Script/Resources/IResource.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public enum ResourceType
 {
-
+	Text, Image, Sequence, Video
 }
 
-public abstract class IResource  {
+public abstract class IResource : IConfigable  {
+
+	protected byte[] fileRef;
+	protected string Path;
 
     public abstract ResourceType GetType();
     public abstract string GetPath();
 
+	public abstract object GetFile ();
+
+	public abstract void LoadConfig (object config);
+
+	public abstract void SetConfig ();
+
+	public abstract void SetDefaultConfig ();
 }
---
+++ SspUnityProject/Assets/Script/Resources/TextResoure.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextResoure : IResource {
+
+	public override void SetConfig ()
+	{
+		
+	}
+
+	public override void LoadConfig (object config)
+	{
+		
+	}
+
+	public override string GetPath ()
+	{
+		return Path;
+	}
+
+	public override void SetDefaultConfig ()
+	{
+		
+	}
+
+	public override object GetFile ()
+	{
+		if (null == fileRef) {
+			return null;
+		}
+
+		return (object)System.Text.Encoding.Default.GetString ( fileRef );
+	}
+
+	IEnumerator LoadFile()
+	{
+		WaitUntil wait = new WaitUntil (() => {
+			fileRef = System.IO.File.ReadAllBytes (Path);
+			return fileRef != null;
+		});
+		yield return wait;
+	}
+
+	public override ResourceType GetType ()
+	{
+		return ResourceType.Text;
+	}
+}
